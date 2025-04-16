6ee1f00c11e965053ac2b54e0ba1d603e1ec245b
Godhani  Ankur <ankurgodhani218@gmail.com>
2025-04-16 15:15:21 +0530

    add getallbutton.

--- Backend1.0/Router/ButtonRouter.js
+++ Backend1.0/Router/ButtonRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { basicbuttom, incrementLikeAndView } = require('../controller/Buttonsection');
+const { basicbuttom, incrementLikeAndView, getAllButtons } = require('../controller/Buttonsection');
 
 router.post('/basicbutton', basicbuttom);
+router.get('/getbasicbutton',getAllButtons);
 router.post('/like-view',incrementLikeAndView)
 
 exports.ButtonRouter = router;
